2dd32fe2381dfec4efb6afd0696753c1b6cf0420
edwardsuwirya <edward_suwirya@yahoo.com>
2022-09-02 21:12:03 +0700

    React Nginx Conf

--- dockerfile
+++ dockerfile
@@ -5,5 +5,7 @@ RUN npm install
 RUN npm run build
 
 FROM nginx:stable-alpine
-
+COPY default.conf.template /etc/nginx/templates/
+ENV API_HOST=0.0.0.0
+ENV API_PORT=8888
 COPY --from=build-step /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Product
-      </header>
-    </div>
-  );
+const App = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const getProduct = async () => {
+            const response = await axios.get('api/products');
+            const productResults = response.data.message;
+            setProducts(productResults);
+        }
+        getProduct();
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                {products.map((p) => (
+                    <div key={p.Id}>{p.ProductName} {p.CategoryName}</div>
+                ))}
+            </header>
+        </div>
+    );
 }
 
 export default App;
